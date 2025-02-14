0db5378be1b96b1bb8c79c5cb0f3249734bea565
ShraddhaAbhang <shraddhasampatabhang@gmail.com>
2025-02-14 20:26:16 +0530

    added code for text extraction from img

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import pytesseract
+
+# Add this line
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+# Now, load the image and extract text
+from PIL import Image
+image_path = "delivery_challan_sample.jpeg"
+img = Image.open(image_path)
+text = pytesseract.image_to_string(img)
+print(text)
